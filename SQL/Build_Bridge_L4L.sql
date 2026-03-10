d00dc5a6a3147d9f0da390ed9abb7480a18fb814
Salvatore Cagliari <salvatorecagliari@gmail.com>
2026-03-10 16:29:36 +0100

    Added logic PY calculation
    Added L4LKey_PY column.
    Started development of SQL query Bride_L4L table

---
+++ SQL/Build_Bridge_L4L.sql
@@ -0,0 +1,73 @@
+WITH [Source]
+AS
+(SELECT [St].[StoreKey]
+        --,[St].[GeographyKey]
+        ,[St].[Status]
+        ,DATEADD(yyyy, 13, CONVERT(date, [St].[OpenDate])) AS  [OpenDate]
+    FROM [dbo].[DimStore]   AS  [St]
+        INNER JOIN [dbo].[DimGeography]     AS  [G]
+            ON [G].[GeographyKey] = [St].[GeographyKey]
+        WHERE [G].[RegionCountryName] = 'Italy')
+, [Stores]
+AS
+(SELECT [StoreKey]
+        --,[GeographyKey]
+        ,CASE [StoreKey]
+            WHEN 225
+                THEN CONVERT(date, '20240713')
+            WHEN 224
+                THEN CONVERT(date, '20240524')
+            WHEN 226
+                THEN CONVERT(date, '20241018')
+            ELSE [OpenDate]
+         END                AS  [OpenDate]
+        ,CASE [StoreKey]
+            WHEN 222
+                THEN CONVERT(date, '20240818')
+            WHEN 225
+                THEN CONVERT(date, '20240303')
+            ELSE CONVERT(date, '20301231')
+         END                AS  [CloseDate]
+        ,CASE [StoreKey]
+            WHEN 224
+                THEN 2  --  Opening
+            WHEN 226
+                THEN 2  --  Opening
+            WHEN 222
+                THEN 3  --  Closing
+            WHEN 225
+                THEN 4  --  Refresh
+            ELSE 1      --  Comparable
+         END                AS  [L4LKey]
+    FROM [Source]
+    UNION ALL
+    SELECT [StoreKey]
+            ,CONVERT(date, '20231013')  AS  [OpenDate]
+            ,CONVERT(date, '20230814')  AS  [CloseDate]
+            ,4                          AS  [L4LKey]
+        FROM [Source]
+            WHERE [StoreKey] = 222
+    )
+SELECT [StoreKey]
+        --,[GeographyKey]
+        ,[OpenDate]
+        ,[CloseDate]
+        --,EOMONTH([OpenDate])    AS  [OpenDate_EOM]
+        --,EOMONTH([CloseDate])   AS  [CloseDate_EOM]
+        ,[L4LKey]
+        ,1
+    FROM [Result]
+    WHERE [StoreKey] != 223
+    )
+, [Months]
+AS
+(
+SELECT DISTINCT 
+      [MonthKey]
+      ,[MonthKeyPY]
+      ,[FirstDayOfMonthKey]
+      ,[LastDayOfMonthKey]
+      ,[FirstDayOfMonthPYKey]
+      ,[LastDayOfMonthPYKey]
+  FROM [dbo].[Date])
+
